ffa50c8ca53651759976fff479f8016e12c2f3a3
ValerioGoffry <67636564+ValerioGoffry@users.noreply.github.com>
2022-07-09 16:17:37 +0200

    Standard_Library
    Lezione Standard Library

---
+++ Standard_Library.py
@@ -0,0 +1,12 @@
+import random
+from math import sqrt
+
+from os import *
+
+for x in range(10):
+    numero = random.randint(1, 100)
+    print("numero:",numero)
+
+print((sqrt(169)))
+
+print(getcwd())
