8c7c27d65cf025abfe70de588e81883fde33d02a
ValerioGoffry <67636564+ValerioGoffry@users.noreply.github.com>
2022-07-09 14:30:19 +0200

    Ciclo_While
    Lezione Ciclo While

--- 9_Il_Ciclo_While.py
+++ 9_Il_Ciclo_While.py
@@ -1,2 +1,25 @@
 #Ciclo While
 
+contatore = 0
+
+# if contatore <= 10:
+#     print(contatore)
+#     contatore += 1 # contatore = contatore + 1
+
+
+# while contatore <= 10:
+#     print(contatore)
+#     contatore += 1 # contatore = contatore + 1
+#     if contatore == 5:
+#         print(contatore)
+#         print("Sto uscendo dal loop...")
+#         break
+
+while contatore <= 10:
+    contatore += 1 # contatore = contatore + 1
+    if contatore == 5:
+        print("saltato")
+        continue
+    print(contatore)
+
+
