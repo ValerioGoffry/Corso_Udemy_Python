86476c8170fdf80accd574d84d2f4a030559c4b6
ValerioGoffry <67636564+ValerioGoffry@users.noreply.github.com>
2022-07-09 14:48:00 +0200

    Ciclo_For
    Lezione Ciclo For

---
+++ 10_Il_Ciclo_For.py
@@ -0,0 +1,16 @@
+#Ciclo For
+
+# contatore = 0
+#
+# while contatore <=10:
+#     print(contatore)
+#     contatore += 1
+
+# for numero in range(11):
+#     print(numero)
+
+# for numero in range(2, 11, 2):
+#     print(numero)
+
+for numero in range(10, 0, -1):
+    print(numero)
