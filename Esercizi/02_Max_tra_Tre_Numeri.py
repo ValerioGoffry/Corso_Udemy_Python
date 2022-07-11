ae6396984d22a0ccde490b596bc75197b623811d
ValerioGoffry <67636564+ValerioGoffry@users.noreply.github.com>
2022-07-11 10:58:25 +0200

    Update 02_Max_tra_Tre_Numeri.py

--- Esercizi/02_Max_tra_Tre_Numeri.py
+++ Esercizi/02_Max_tra_Tre_Numeri.py
@@ -2,7 +2,7 @@
 
 def max_tra_tre_Numeri(a,b,c):
     if a == b == c:
-        print("I numeri sono identici")
+        print("I numeri sono identici.")
     elif a >= b and a >= c:
         print("Il numero più grande è a:", a)
         return a
