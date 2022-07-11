da15f51a4db1e4392d215028774375d18aa67379
ValerioGoffry <67636564+ValerioGoffry@users.noreply.github.com>
2022-07-11 17:57:45 +0200

    .

--- 14_Le_Liste.py
+++ 14_Le_Liste.py
@@ -1,4 +1,4 @@
-#Le Liste
+# Le Liste
 
 # elenco = [2, 5, "bacon", 3.14, "eggs"]
 #
@@ -21,23 +21,23 @@
 # mia_lista = list(range(49, 155))
 # # print(mia_lista)
 
-altra_lista = [2, 5, "bacon", 3.14,["eggs", "asd", 55, 23]]
+altra_lista = [2, 5, "bacon", 3.14, ["eggs", "asd", 55, 23]]
 print(altra_lista[4][1])
 altra_lista[1] = 6
 print(altra_lista)
 
-del altra_lista[1] # eliminare un elemento in una lista tramite indice
+del altra_lista[1]  # eliminare un elemento in una lista tramite indice
 print(altra_lista)
 
-altra_lista.remove(3.14)# eliminare un elemento in una lista tramite l elemento/valore
+altra_lista.remove(3.14)  # eliminare un elemento in una lista tramite l elemento/valore
 print(altra_lista)
 
-altra_lista.append("spam") #aggiungere un elemento in una lista
+altra_lista.append("spam")  # aggiungere un elemento in una lista
 print(altra_lista)
 
-altra_lista.reverse() # inverte la lista
+altra_lista.reverse()  # inverte la lista
 print(altra_lista)
 
-lista_numerica = [3,2,6,7,1,77] #ordina una lista
+lista_numerica = [3, 2, 6, 7, 1, 77]  # ordina una lista
 lista_numerica.sort()
 print(lista_numerica)
