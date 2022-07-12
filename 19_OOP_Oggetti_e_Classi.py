fac7a73497b514c5709bc580fc712569a0c38df0
ValerioGoffry <67636564+ValerioGoffry@users.noreply.github.com>
2022-07-12 18:12:46 +0200

    OOP Oggetti e classi

---
+++ 19_OOP_Oggetti_e_Classi.py
@@ -0,0 +1,22 @@
+# Python - Programmazione a Oggetti
+
+x = 22
+y = 12.3
+mia_l = [1, 2, 3]
+d = {2: 3, 4: 5}
+
+
+# print(type(x))
+# print(type(y))
+# print(type(mia_l))
+# print(type(d))
+#
+# mia_l.append(33)
+# print(mia_l)
+
+class Esempio:
+    pass
+
+
+ogg = Esempio()
+print(type(ogg))
